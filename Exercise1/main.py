51111f701bd1aa8c0c7b976d8cd52cab6540bce5
myleslangston <84974498+myleslangston@users.noreply.github.com>
2021-07-07 14:34:58 +0100

    Practice Python Q1

---
+++ Exercise1/main.py
@@ -0,0 +1,9 @@
+name = input("please insert your name: ")
+age = input("What is your age? ")
+
+year = 2021
+reach_100 = year + (100 - int(age))
+final_print = f"You will reach 100 in the year {reach_100}\n"
+
+number = input("please select a number: ")
+print(final_print * int(number))
